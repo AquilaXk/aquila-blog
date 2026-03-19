d8edef44a89b9d37411cc8406af7b474557dd8e7
AquilaXk <illusiveman7@gmail.com>
2026-03-19 10:35:31 +0900

    chore: checkpoint all current changes

--- back/src/main/resources/db/migration/R__operational_indexes.sql
+++ back/src/main/resources/db/migration/R__operational_indexes.sql
@@ -9,9 +9,15 @@ BEGIN
 
     IF to_regclass('public.member') IS NOT NULL
         AND EXISTS(SELECT 1 FROM pg_extension WHERE extname = 'pgroonga') THEN
-        CREATE INDEX IF NOT EXISTS member_idx_pgroonga_username_nickname
-            ON member USING pgroonga ((ARRAY["username"::text, "nickname"::text])
-            pgroonga_text_array_full_text_search_ops_v2) WITH (tokenizer = 'TokenBigram');
+        IF EXISTS(SELECT 1 FROM pg_opclass WHERE opcname = 'pgroonga_text_array_full_text_search_ops_v2') THEN
+            CREATE INDEX IF NOT EXISTS member_idx_pgroonga_username_nickname
+                ON member USING pgroonga ((ARRAY["username"::text, "nickname"::text])
+                pgroonga_text_array_full_text_search_ops_v2) WITH (tokenizer = 'TokenBigram');
+        ELSIF EXISTS(SELECT 1 FROM pg_opclass WHERE opcname = 'pgroonga_text_array_full_text_search_ops') THEN
+            CREATE INDEX IF NOT EXISTS member_idx_pgroonga_username_nickname
+                ON member USING pgroonga ((ARRAY["username"::text, "nickname"::text])
+                pgroonga_text_array_full_text_search_ops) WITH (tokenizer = 'TokenBigram');
+        END IF;
     END IF;
 END $$;
 
@@ -33,9 +39,15 @@ BEGIN
 
     IF to_regclass('public.post') IS NOT NULL
         AND EXISTS(SELECT 1 FROM pg_extension WHERE extname = 'pgroonga') THEN
-        CREATE INDEX IF NOT EXISTS idx_post_title_content_pgroonga
-            ON post USING pgroonga ((ARRAY["title"::text, "content"::text])
-            pgroonga_text_array_full_text_search_ops_v2) WITH (tokenizer = 'TokenBigram');
+        IF EXISTS(SELECT 1 FROM pg_opclass WHERE opcname = 'pgroonga_text_array_full_text_search_ops_v2') THEN
+            CREATE INDEX IF NOT EXISTS idx_post_title_content_pgroonga
+                ON post USING pgroonga ((ARRAY["title"::text, "content"::text])
+                pgroonga_text_array_full_text_search_ops_v2) WITH (tokenizer = 'TokenBigram');
+        ELSIF EXISTS(SELECT 1 FROM pg_opclass WHERE opcname = 'pgroonga_text_array_full_text_search_ops') THEN
+            CREATE INDEX IF NOT EXISTS idx_post_title_content_pgroonga
+                ON post USING pgroonga ((ARRAY["title"::text, "content"::text])
+                pgroonga_text_array_full_text_search_ops) WITH (tokenizer = 'TokenBigram');
+        END IF;
     END IF;
 END $$;
 

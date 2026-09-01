77e4e0ae31abb917d956e72ae885f2cdcf141789
AquilaXk <illusiveman7@gmail.com>
2026-09-01 15:45:31 +0900

    fix(member): mark custom repository as functional

--- back/src/main/kotlin/com/back/boundedContexts/member/adapter/persistence/MemberRepositoryCustom.kt
+++ back/src/main/kotlin/com/back/boundedContexts/member/adapter/persistence/MemberRepositoryCustom.kt
@@ -2,6 +2,6 @@ package com.back.boundedContexts.member.adapter.persistence
 
 import com.back.boundedContexts.member.domain.shared.Member
 
-interface MemberRepositoryCustom {
+fun interface MemberRepositoryCustom {
     fun findByLoginId(loginId: String): Member?
 }
